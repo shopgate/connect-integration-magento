93928441932b2c2ff27313b72210e4efe17a9fa1
Alexander Wesselburg <info@myportal.de>
2017-11-21 10:17:18 +0100

    WEBC-450 - create-new-table-to-be-able-to-flag

---
+++ src/app/code/community/Shopgate/Cloudapi/sql/shopgate_cloudapi_setup/mysql4-upgrade-0.1.1-0.1.2.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright Shopgate Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author    Shopgate Inc, 804 Congress Ave, Austin, Texas 78701 <interfaces@shopgate.com>
+ * @copyright Shopgate Inc
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ */
+
+/** @var Shopgate_Cloudapi_Model_Resource_Setup $installer */
+$installer = $this;
+$installer->startSetup();
+
+$this->run("
+CREATE TABLE IF NOT EXISTS `{$this->getTable('shopgate_order_sources')}` (
+ `entity_id` int(11) NOT NULL AUTO_INCREMENT,
+ `order_id` int(11) NOT NULL,
+ `source` ENUM ('webcheckout'),
+ PRIMARY KEY (`entity_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+");
+
+$installer->endSetup();
