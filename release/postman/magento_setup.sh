d5251b12eb402e82b1402c0d9a346d0be86f607d
Konstantin Kiritsenko <konstantin.kiritsenko@shopgate.com>
2017-11-24 21:27:14 +0800

    WEBC-442 n98 testing
    Signed-off-by: Konstantin Kiritsenko <konstantin.kiritsenko@shopgate.com>

--- release/postman/magento_setup.sh
+++ release/postman/magento_setup.sh
@@ -1,13 +1,39 @@
 #!/bin/sh
 
-# Apache related
 cd ${WEB_PATH}
-mv ${TRAVIS_BUILD_DIR}/release/postman/info.php ${WEB_PATH}
 
-curl localhost/info.php
+# todo-sg: create a travis matrix of folder_name->repo_name
+# check if cache exists
+if [ ! -d "${MAGE_FOLDER1}" ];
+then
+	DOWNLOAD="-v"
+else
+	DOWNLOAD="--noDownload"
+fi
 
-mysql -e 'CREATE DATABASE magento1;'
+n98 script:repo:run n98-setup \
+-d folder=${MAGE_FOLDER1} \
+-d api_key=${CFG_API_KEY} \
+-d shop_number=${CFG_API_SHOP_NUMBER} \
+-d customer_number=${CFG_API_CUSTOMER_NUMBER} \
+-d user1_email=${USER1_EMAIL} \
+-d user2_email=${USER2_EMAIL} \
+-d user3_email=${USER3_EMAIL} \
+-d user_pass=${USER_PASS} \
+-d no_download=${DOWNLOAD}
 
-# N98 related
-chmod +x ${TRAVIS_BUILD_DIR}/release/postman/n98-setup.magerun
-n98 script < ${TRAVIS_BUILD_DIR}/release/postman/n98-setup.magerun
+# todo-sg: atm does not pass the right info, just use newman
+curl -X POST \
+  http://127.0.0.1/${MAGE_FOLDER1}/shopgate/v2/auth/token \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H "php_auth_user: ${CFG_API_CUSTOMER_NUMBER}-${CFG_API_SHOP_NUMBER}" \
+  -H "php_auth_pw: ${CFG_API_KEY}" \
+  -d "{
+  'grant_type': 'password',
+  'username': '${USER1_EMAIL}',
+  'password': '${USER_PASS}'
+}"
+
+# return back
+cd ${TRAVIS_BUILD_DIR}
