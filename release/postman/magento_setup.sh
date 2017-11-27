2fe1a982b2cc8b1b753abc14b721f5ad85fc7513
Konstantin Kiritsenko <konstantin.kiritsenko@shopgate.com>
2017-11-27 16:16:22 +0800

    WEBC-442 newMan run
    Signed-off-by: Konstantin Kiritsenko <konstantin.kiritsenko@shopgate.com>

--- release/postman/magento_setup.sh
+++ release/postman/magento_setup.sh
@@ -3,7 +3,7 @@
 cd ${WEB_PATH}
 
 # todo-sg: create a travis matrix of folder_name->repo_name
-# check if cache exists
+# todo-sg: enable travis caching and configure it to now download all the time
 if [ ! -d "${MAGE_FOLDER1}" ];
 then
 	DOWNLOAD="-v"
@@ -20,20 +20,9 @@ n98 script:repo:run n98-setup \
 -d user2_email=${USER2_EMAIL} \
 -d user3_email=${USER3_EMAIL} \
 -d user_pass=${USER_PASS} \
--d no_download=${DOWNLOAD}
+-d no_download=${DOWNLOAD} > /dev/null 2>&1
 
-# todo-sg: atm does not pass the right info, just use newman
-curl -X POST \
-  http://127.0.0.1/${MAGE_FOLDER1}/shopgate/v2/auth/token \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' \
-  -H "php_auth_user: ${CFG_API_CUSTOMER_NUMBER}-${CFG_API_SHOP_NUMBER}" \
-  -H "php_auth_pw: ${CFG_API_KEY}" \
-  -d "{
-  'grant_type': 'password',
-  'username': '${USER1_EMAIL}',
-  'password': '${USER_PASS}'
-}"
+sudo chmod 777 -R ${MAGE_FOLDER1}/var
 
 # return back
 cd ${TRAVIS_BUILD_DIR}
