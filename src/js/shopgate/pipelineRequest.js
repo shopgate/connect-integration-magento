beddd3466ef5df7b7ae2ba0ecd11e30fe412543e
Alexander Wesselburg <info@myportal.de>
2017-11-27 15:26:32 +0100

    add meta field and SGEvent Object

--- src/js/shopgate/pipelineRequest.js
+++ src/js/shopgate/pipelineRequest.js
@@ -18,3 +18,27 @@ var ShopgatePipelineRequest = {
         }
     }
 }
+
+SGEvent = {
+    __call: function (eventName, eventArguments) {
+
+        console.log('Received event ' + eventName + ' with args ' + JSON.stringify(eventArguments));
+
+        if(!eventArguments || !Array.isArray(eventArguments)) {
+            eventArguments = [];
+        }
+
+        if(SGEvent[eventName]) {
+            SGEvent[eventName].apply(SGEvent, eventArguments);
+        }
+    },
+
+    /**
+     * This is event is called by the app to recognize if the lib is ready
+     *
+     * @returns {boolean}
+     */
+    isDocumentReady: function() {
+        return true;
+    }
+};
