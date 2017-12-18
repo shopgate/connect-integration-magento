61f1ae5e7c4e33efe894388c51bce08526f0aba1
Konstantin Kiritsenko <k@componentk.com>
2017-12-18 18:46:12 +0800

    WEBC-512 Implementing Locale testing
    Signed-off-by: Konstantin Kiritsenko <k@componentk.com>

--- tests/postman/test_env_setup.sh
+++ tests/postman/test_env_setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
+
+# Install Apache & Enable php-fpm
 sudo apt-get update
 sudo apt-get install apache2 libapache2-mod-fastcgi
-# enable php-fpm
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 sudo a2enmod rewrite actions fastcgi alias
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
@@ -11,12 +12,14 @@ echo "always_populate_raw_post_data=-1" >> ~/.phpenv/versions/$(phpenv version-n
 sudo sed -i -e "s,www-data,travis,g" /etc/apache2/envvars
 sudo chown -R travis:travis /var/lib/apache2/fastcgi
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+
 # configure apache virtual hosts
 sudo cp -f ./tests/postman/travis-ci-apache /etc/apache2/sites-available/default
 sudo chown -R travis:travis ${WEB_PATH}
 sudo chmod 750 ${WEB_PATH}
 sudo chown -R travis:travis /var/lock/apache2/
 sudo service apache2 restart
+
 # n98 related
 wget --quiet https://files.magerun.net/n98-magerun.phar
 chmod +x ./n98-magerun.phar
@@ -27,5 +30,13 @@ sudo chown -R travis:travis /usr/local/share/n98-magerun
 sudo chmod +x /usr/local/share/n98-magerun/scripts/*.magerun
 sudo cp -a ./tests/postman/.n98-magerun.yaml ~/
 sudo chmod +x ~/.n98-magerun.yaml
+
+if [[ ${MAGE_LOCALE} == "en_US" ]];
+then
+	sudo sed -e "s?EUR?USD?g" --in-place ~/.n98-magerun.yaml
+	sudo sed -e "s?de_DE?${MAGE_LOCALE}?g" --in-place ~/.n98-magerun.yaml
+	echo 'subbed default config values'
+fi
+
 # newman related
 npm install newman --global
