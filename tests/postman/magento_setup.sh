222c325eb6440ad013d7071bdcdd5b329fc40a74
Rainer Skistims <rainer.skistims@shopgate.com>
2018-09-14 10:45:09 +0200

    MAGENTO-2114 run sql to add custom attribute for tests before mage installation

--- tests/postman/magento_setup.sh
+++ tests/postman/magento_setup.sh
@@ -16,6 +16,7 @@ if [[ ${MAGE_TYPE} == "EE" ]]; then
 	rsync -a ./sample/magento-sample-data-1.14.2.4/ ./${MAGE_FOLDER}
 	mysql -e "CREATE database ${MAGE_FOLDER};"
 	mysql ${MAGE_FOLDER} < ./${MAGE_FOLDER}/magento_sample_data_for_1.14.2.4.sql
+	mysql ${MAGE_FOLDER} < ${TRAVIS_BUILD_DIR}/tests/postman/sql/addCustomAttribute.sql
 	DOWNLOAD="--noDownload"
 	PARAM2="--forceUseDb"
 	MAGE_PACKAGE="magento-mirror-1.9.3.6" # package needs to exist for n98 to accept install
@@ -40,7 +41,6 @@ mysql ${MAGE_FOLDER} -e "DELETE FROM eav_attribute WHERE backend_model='catalog/
 mysql ${MAGE_FOLDER} -e "DELETE FROM eav_attribute WHERE backend_model='enterprise_catalog/product_attribute_backend_urlkey';"
 
 # Adds app-only coupon
-mysql ${MAGE_FOLDER} < ${TRAVIS_BUILD_DIR}/tests/postman/sql/addCustomAttribute.sql
 mysql ${MAGE_FOLDER} < ${TRAVIS_BUILD_DIR}/tests/postman/sql/addMobileAppCartRule.sql
 mysql ${MAGE_FOLDER} < ${TRAVIS_BUILD_DIR}/tests/postman/sql/addSoapUser.sql
 
