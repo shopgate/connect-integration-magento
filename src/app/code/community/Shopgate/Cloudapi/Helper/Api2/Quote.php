6b7c475764a37172060902e0b21c285c1d46f41b
Andre Kraus <andre.kraus@shopgate.com>
2019-01-25 08:58:40 +0100

    MAGENTO-2330: added missing constant

--- src/app/code/community/Shopgate/Cloudapi/Helper/Api2/Quote.php
+++ src/app/code/community/Shopgate/Cloudapi/Helper/Api2/Quote.php
@@ -26,6 +26,7 @@ class Shopgate_Cloudapi_Helper_Api2_Quote extends Mage_Core_Helper_Abstract
     const KEY_TOTALS                      = 'totals';
     const KEY_ERROR_MESSAGES              = 'errors';
     const KEY_ERRORS                      = 'has_error';
+    const KEY_SHIPPING                    = 'shipping';
     const KEY_CART_PRICE_DISPLAY_SETTINGS = 'cart_price_display_settings';
 
     /**
