abf9f79272f0e91cc6d1063ff6391feee443aae1
Andre Kraus <andre.kraus@shopgate.com>
2019-01-28 15:40:08 +0100

    MAGENTO-2347: added postman tests for adding custom option products

---
+++ tests/postman/sql/addCustomOption.sql
@@ -0,0 +1,9 @@
+INSERT INTO `catalog_product_option` (`product_id`, `type`, `is_require`, `sku`, `max_characters`, `file_extension`, `image_size_x`, `image_size_y`, `sort_order`)
+VALUES
+	(553, 'field', 1, NULL, 10, NULL, NULL, NULL, 0);
+
+SET @OPTION_ID=LAST_INSERT_ID();
+
+INSERT INTO `catalog_product_option_title` (`option_id`, `store_id`, `title`)
+VALUES
+	(@OPTION_ID, 0, 'Engraving');
