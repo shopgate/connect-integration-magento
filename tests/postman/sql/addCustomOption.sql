7b117496fd0739e46c5b02f3ff5f69de9716798c
Konstantin Kiritsenko <k@componentk.com>
2019-02-04 12:40:19 +0700

    MAGENTO-2347 Adding error description + enhancements
    Signed-off-by: Konstantin Kiritsenko <k@componentk.com>

--- tests/postman/sql/addCustomOption.sql
+++ tests/postman/sql/addCustomOption.sql
@@ -1,9 +1,10 @@
 INSERT INTO `catalog_product_option` (`product_id`, `type`, `is_require`, `sku`, `max_characters`, `file_extension`, `image_size_x`, `image_size_y`, `sort_order`)
-VALUES
-	(553, 'field', 1, NULL, 10, NULL, NULL, NULL, 0);
+VALUES (553, 'field', 1, 'engraving-sku', 14, null, null, null, 10);
 
 SET @OPTION_ID=LAST_INSERT_ID();
 
 INSERT INTO `catalog_product_option_title` (`option_id`, `store_id`, `title`)
-VALUES
-	(@OPTION_ID, 0, 'Engraving');
+VALUES (@OPTION_ID, 0, 'Engraving');
+
+INSERT INTO `catalog_product_option_price` (`option_id`, `store_id`, `price`, `price_type`)
+VALUES (@OPTION_ID, 0, 5.0000, 'fixed');
