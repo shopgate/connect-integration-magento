ec47c3967b7c2ef0a0f84222abece4481eb58604
Konstantin Kiritsenko <k@componentk.com>
2019-03-13 08:07:52 +0700

    MAGENTO-2414 Swagger fixes + travis swagger validation + badge on master
    Signed-off-by: Konstantin Kiritsenko <k@componentk.com>

---
+++ tests/swagger/validator.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# NOTE: This file is modified version taken from:
+# https://github.com/amardeshbd/medium-api-specification/blob/master/api-spec_validation_test.sh
+
+# Tests the swagger specificaton using online service
+testOpenApiSpecValidity() {
+    expectedOutput="{}"
+    expectedOutputSize=${#expectedOutput}
+
+    # Prepares the spec URL from GitHub Pull-Request (PR)
+    specUrl="https://raw.githubusercontent.com/$TRAVIS_REPO_SLUG/$BRANCH/docs/swagger.yaml"
+    # Now prepare the open API spec file to use the online validator service.
+    validationUrl="http://online.swagger.io/validator/debug?url=$specUrl"
+
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+    echo "Validating ENV Variables: TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+    echo "Spec URL: ${specUrl}"
+    echo "OpenAPI Specification File=$validationUrl"
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+
+    validationOutput=$(curl $validationUrl)
+    validationOutputSize=${#validationOutput}
+    echo "Testing swagger validation - current output is: $validationOutput"
+    echo "Expected valid size: $expectedOutputSize, current output: $validationOutputSize"
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+
+    assertEquals "Validation failed - service unavailable or error found." $expectedOutputSize $validationOutputSize
+}
+
+
+# Execute shunit2 to run the tests (downloaded via `.travis.yaml`)
+. shunit2-2.1.6/src/shunit2
