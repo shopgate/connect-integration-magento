12c5ae0290049722498e5ab5d52b89f579ec9328
André Kraus <andre.kraus@shopgate.com>
2020-05-19 09:10:59 +0000

    MAGENTO-2791: removed expiration date for salesrule

--- tests/postman/sql/addMobileAppCartRule.sql
+++ tests/postman/sql/addMobileAppCartRule.sql
@@ -14,7 +14,7 @@ INSERT INTO `salesrule_customer_group` (`rule_id`, `customer_group_id`)
   (LAST_INSERT_ID(),1);
 
 INSERT INTO `salesrule_coupon` (`rule_id`, `code`, `usage_limit`, `usage_per_customer`, `times_used`, `expiration_date`, `is_primary`, `created_at`, `type`)
-VALUES (LAST_INSERT_ID(), 'APP10', NULL, NULL, 0, '2099-02-25 00:00:00', 1, '2018-08-01 00:00:00', 0);
+VALUES (LAST_INSERT_ID(), 'APP10', NULL, NULL, 0, NULL, 1, '2018-08-01 00:00:00', 0);
 
 /**
  * Insert non-coupon sales rule to test existing carts that were not created by us
