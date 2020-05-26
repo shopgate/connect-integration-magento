5bd1d4c540635524527dddda0cb29df37cd4e18b
André Kraus <andre.kraus@shopgate.com>
2020-05-26 06:46:02 +0000

    MAGENTO-2791: extended travis logs

--- tests/postman/magento_setup.sh
+++ tests/postman/magento_setup.sh
@@ -6,7 +6,7 @@ cd ${WEB_PATH}
 DOWNLOAD=""
 PARAM2=""
 if [[ ${MAGE_TYPE} == "EE" ]]; then
-	~/.local/bin/aws s3api get-object --bucket shopgate-ci --key magento/magento-shoppingsystem-files.tar.gz magento.tar.gz > /dev/null 2>&1
+	~/.local/bin/aws s3api get-object --bucket shopgate-ci --key magento/magento-shoppingsystem-files.tar.gz magento.tar.gz
 	tar -xzf magento.tar.gz ./
 	tar -xzf ./package/ee/${MAGE_PACKAGE}.tar.gz -C ./
 	mv ./${MAGE_PACKAGE} ./${MAGE_FOLDER}
